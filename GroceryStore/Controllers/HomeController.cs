78cb6c45c6e2b95c1a812f5e66e55e0adb04e042
maria-lau <m.marialau@gmail.com>
2018-03-30 14:14:02 -0600

    Work in progress.

--- GroceryStore/Controllers/HomeController.cs
+++ GroceryStore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GroceryStore.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This is a ";
 
             return View();
         }
