2e55cb3fe5c7e808ba862520868e83c7e85abf52
juan-nieto <nieto.juan15@gmail.com>
2018-04-07 19:12:26 -0600

    PlaceOrder function fixed, to be merged with Maria branch

--- GroceryStore/Models/Cart.cs
+++ GroceryStore/Models/Cart.cs
@@ -10,19 +10,19 @@ namespace GroceryStore.Models
         public Cart()
         {
             // Each tuple in the list will have <int SKU, int quantity>
-           cartcontents = new List<Tuple<int, int>>();
+          // cartcontents = new List<Tuple<int, string, int>>();
         }
 
-        public List<Tuple<int, int>> cartcontents { get; set; }
+        public List<Tuple<int, string, int>> cartcontents { get; set; }
 
-        public void AddtoCart(int SKU, int quantity)
+        public void AddtoCart(int SKU, int quantity, string itemname)
         {
-            cartcontents.Add(Tuple.Create(SKU, quantity));
+            cartcontents.Add(Tuple.Create(SKU, itemname, quantity));
         }
 
-        public void DeleteFromCart(int SKU, int quantity)
+        public void DeleteFromCart(int SKU, int quantity, string itemname)
         {
-            cartcontents.RemoveAt(cartcontents.IndexOf(Tuple.Create(SKU, quantity)));
+            cartcontents.RemoveAt(cartcontents.IndexOf(Tuple.Create(SKU, itemname, quantity)));
         }
     }
 }
